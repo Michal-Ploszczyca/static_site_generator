95b3e2ab86e525d1feaa6877c240e70341bf79c6
Michal-Ploszczyca <michal.ploszczyca98@gmail.com>
2024-06-19 15:12:06 +0200

    Initial commit

---
+++ src/test_textnode.py
@@ -0,0 +1,36 @@
+import unittest
+
+from textnode import TextNode
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node, node2)
+
+
+    def test_not_eq(self):
+        node = TextNode("This is a text node", "italic")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertNotEqual(node, node2)
+
+
+    def test_diff_text(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a different text node", "bold")
+        self.assertNotEqual(node, node2)
+
+    def test_url_property_none(self):
+        node  = TextNode("This is a text node", "bold", url=None)
+        node2 = TextNode("This is a text node", "bold", url= "http://example.com")
+        self.assertNotEqual(node, node2)
+
+    def test_trailing_spaces(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node ", "bold")  #space at the end
+        self.assertNotEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
